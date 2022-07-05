851a0dffc9230e7160db63fb2ddc8b6d15ce0194
Erick Chávez <ericksebastianchavezgarcia4gmail.com>
2022-07-04 20:16:47 -0500

    Taller 17 - JPA y mapeo de secuencias

--- src/main/java/com/uce/edu/demo/ProyectoU2EcApplication.java
+++ src/main/java/com/uce/edu/demo/ProyectoU2EcApplication.java
@@ -6,18 +6,17 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.uce.edu.demo.repository.modelo.Estudiante;
-import com.uce.edu.demo.service.IEstudianteService;
+import com.uce.edu.demo.repository.modelo.Persona;
 import com.uce.edu.demo.service.IPersonaJPAService;
 
 @SpringBootApplication
-public class ProyectoU2EcApplication implements CommandLineRunner{
-	
+public class ProyectoU2EcApplication implements CommandLineRunner {
+
 	private static Logger logJava = Logger.getLogger(ProyectoU2EcApplication.class);
-	
+
 	@Autowired
-	private IEstudianteService estudianteService;
-	
+	private IPersonaJPAService personaJPAService;
+
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoU2EcApplication.class, args);
 	}
@@ -25,32 +24,24 @@ public class ProyectoU2EcApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
-		
-		Estudiante estudiante = new Estudiante();
-		estudiante.setId(90);
-		estudiante.setNombre("Carlos");
-		estudiante.setApellido("Carrera");
-		estudiante.setFacultad("Facultad de Medicina");
-		estudiante.setCarrera("Enfermería");
-		
-		//this.estudianteService.insertarEstudiante(estudiante);
-		logJava.info("Se ha ingresado el estudiante: " + estudiante);
-		
-		logJava.info("El estudiante buscado es: " + this.estudianteService.buscarEstudiante(5));
-		
-		Estudiante estudiante2 =  new Estudiante();
-		estudiante2.setId(12);
-		estudiante2.setNombre("Ana");
-		estudiante2.setApellido("Garcia");
-		estudiante2.setFacultad("Facultad de Derecho");
-		estudiante2.setCarrera("Derecho");
-		
-		this.estudianteService.actualizarEstudiante(estudiante2);
-		logJava.info("Se ha actualizado el estudiante : " + estudiante2);
-		
-		this.estudianteService.eliminarEstudiante(90);
-		logJava.info("Se ha eliminado el estudiante");
-		
+
+		//logJava.info("Con JPA: " + this.personaJPAService.buscarId(4));
+
+		Persona persona = new Persona();
+		//persona.setId(20);
+		persona.setNombre("Domenica");
+		persona.setApellido("Erazo");
+
+		this.personaJPAService.insertar(persona);
+		logJava.info("Se ha insertado: " + persona);
+
+		/*persona.setNombre("Kevin");
+		this.personaJPAService.actualizar(persona);
+		logJava.info("Se ha actualizado: " + persona);
+
+		this.personaJPAService.eliminar(1);
+		logJava.info("Se ha eliminado");*/
+
 	}
 
 }
